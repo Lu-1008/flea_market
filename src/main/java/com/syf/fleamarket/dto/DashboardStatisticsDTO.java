f678920bf8510f57da74cc2f81116cbc12e11b7a
开发者A <20233005499@hainanu.edu.cn>
2026-06-02 17:50:01 +0800

    feat(开发者A): 第3周 - 文件上传、数据统计

---
+++ src/main/java/com/syf/fleamarket/dto/DashboardStatisticsDTO.java
@@ -0,0 +1,31 @@
+package com.syf.fleamarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 首页统计数据DTO
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardStatisticsDTO {
+    
+    /**
+     * 用户总数
+     */
+    private Integer userCount;
+    
+    /**
+     * 商品总数
+     */
+    private Integer itemCount;
+    
+    /**
+     * 总销售额
+     */
+    private Double salesAmount;
+} 
